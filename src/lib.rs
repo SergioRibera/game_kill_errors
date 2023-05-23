332246b8def5bb5a525c27cad34cab7c3e306c41
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-05-23 02:32:40 -0400

    feat: add game state, make pickable camera and remade env getters

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 use bevy::{core_pipeline::clear_color::ClearColorConfig, prelude::*, window::WindowMode};
 #[cfg(feature = "inspect")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy_mod_picking::{prelude::RaycastPickCamera, DefaultPickingPlugins};
 use bevy_tweening::TweeningPlugin;
+use game::Game;
 use lazy_static::lazy_static;
 
 use ui::UiPlugin;
 
+mod ext;
 mod game;
 mod lens;
 mod ui;
@@ -17,13 +20,24 @@ pub(crate) const HOME_URL: &str = env!("HOME_URL");
 // Game Enviroment Variables
 lazy_static! {
     pub(crate) static ref TIME_WAIT_TO_START: u64 = {
-        let time = env!("TIME_WAIT_TO_START");
+        let time = std::env::var("TIME_WAIT_TO_START").unwrap_or(10.to_string());
         time.parse::<u64>().unwrap_or(10)
+    };
+    pub(crate) static ref MAX_BUGS_ON_SCREEN: usize = {
+        let time = std::env::var("MAX_BUGS_ON_SCREEN").unwrap_or(30.to_string());
+        time.parse::<usize>().unwrap_or(30)
     };
 }
 
 #[derive(Clone, Resource)]
 pub(crate) struct OpenLinkResource(pub Box<fn(&str)>);
+
+#[derive(Clone, Default, Debug, Hash, States, PartialEq, Eq)]
+pub(crate) enum GameState {
+    #[default]
+    MainPage,
+    Game,
+}
 
 pub fn app(fullscreen: bool, open_url: fn(&str)) -> App {
     let mode = if fullscreen {
@@ -39,26 +53,40 @@ pub fn app(fullscreen: bool, open_url: fn(&str)) -> App {
                 mode,
                 title: LAUNCHER_TITLE.to_string(),
                 fit_canvas_to_parent: true,
-                prevent_default_event_handling: false,
+                prevent_default_event_handling: true,
                 present_mode: bevy::window::PresentMode::AutoVsync,
                 decorations: false,
                 ..default()
             }),
             ..default()
         }))
+        .add_plugins(DefaultPickingPlugins.build())
         .add_plugin(TweeningPlugin);
     #[cfg(feature = "inspect")]
     app.add_plugin(WorldInspectorPlugin::new());
-    app.add_startup_system(setup_camera).add_plugin(UiPlugin);
+    app.add_state::<GameState>()
+        .add_startup_system(setup_camera)
+        .add_plugin(UiPlugin)
+        .add_plugin(Game);
 
     app
 }
 
 fn setup_camera(mut cmd: Commands) {
-    cmd.spawn(Camera2dBundle {
-        camera_2d: Camera2d {
-            clear_color: ClearColorConfig::Custom(Color::rgb_u8(227, 227, 227)),
+    cmd.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(0., 0., 25.),
+            camera_3d: Camera3d {
+                clear_color: ClearColorConfig::Custom(Color::rgb_u8(227, 227, 227)),
+                ..default()
+            },
+            ..default()
         },
+        RaycastPickCamera::default(),
+    ));
+
+    cmd.spawn(DirectionalLightBundle {
+        transform: Transform::from_xyz(8., 16., 8.),
         ..default()
     });
 }

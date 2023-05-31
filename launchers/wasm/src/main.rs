611b407a0e1ccb7349fb814b3f82b2d3b1adf3e7
Sergio Ribera <sergioalejandroriberacosta@gmail.com>
2023-05-31 02:49:06 -0400

    feat: get language from url or system lang default

--- launchers/wasm/src/main.rs
+++ launchers/wasm/src/main.rs
@@ -1,5 +1,4 @@
-use bevy::prelude::*;
-use game::LAUNCHER_TITLE;
+use game::{LocaleLangs, LAUNCHER_TITLE};
 use yew::prelude::*;
 
 fn open_url(url: &str) {
@@ -14,6 +13,39 @@ fn set_window_title(title: &str) {
         .set_title(title);
 }
 
+fn get_lang() -> LocaleLangs {
+    if let Some(lang) = web_sys::window()
+        .and_then(|w| w.document())
+        .expect("Unable to get DOM")
+        .location()
+        .expect("Unable to get Location")
+        .hash()
+        .ok()
+        .and_then(|v| if v.is_empty() { None } else { Some(v) })
+        .map(|h| {
+            log::info!("The lang from hash is {h}");
+            if h.to_lowercase().contains("es") {
+                LocaleLangs::ES
+            } else {
+                LocaleLangs::EN
+            }
+        })
+    {
+        log::info!("From Hash: {lang:?}");
+        return lang;
+    }
+    let nav_lang = web_sys::window()
+        .and_then(|w| Some(w.navigator()))
+        .expect("Unable to get navigator")
+        .language()
+        .unwrap();
+    log::info!("The lang is {nav_lang}");
+    if nav_lang.to_lowercase().starts_with("es") {
+        return LocaleLangs::ES;
+    }
+    LocaleLangs::EN
+}
+
 #[function_component(Root)]
 fn view() -> Html {
     set_window_title(LAUNCHER_TITLE);
@@ -24,9 +56,15 @@ fn view() -> Html {
 }
 
 fn main() {
+    #[cfg(feature = "inspect")]
+    wasm_logger::init(
+        wasm_logger::Config::new(log::Level::Info)
+            // .module_prefix("wasm_kill_errors")
+            // .module_prefix("game"),
+    );
     // Mount the DOM
     yew::Renderer::<Root>::new().render();
     // Start the Bevy App
-    info!("Starting launcher: WASM");
-    game::app(false, open_url).run();
+    log::info!("Starting launcher: WASM");
+    game::app(false, get_lang(), open_url).run();
 }
